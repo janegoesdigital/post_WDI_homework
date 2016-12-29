c9acb551899bcb5b0287eb66e42a9617be097547
jadarling <janeodarling@gmail.com>
2016-12-29 17:12:55 +1100

    Completed the word game with hangman functionality

--- functions_arrays/guess_letter.js
+++ functions_arrays/guess_letter.js
@@ -16,17 +16,25 @@
 // Every time a letter is guessed, generate a random amount and reward the user if they found a letter (multiplying the reward if multiple letters found), otherwise subtract from their reward.
 // When they guess the word, log their final reward amount.
 
-//Third part
+//Third part - like hangman
 
 // Keep track of all the guessed letters (right and wrong) and only let the user guess a letter once. If they guess a letter twice, do nothing.
 // Keep track of the state of the hangman as a number (starting at 0), and subtract or add to that number every time they make a wrong guess.
 // Once the number reaches 6 (a reasonable number of body parts for a hangman), inform the user that they lost and show a hangman on the log.
 
 
+
+// Keep track of the state of the hangman as a number (starting at 0), and subtract or add to that number every time they make a wrong guess.
+
+
+
+
+
 var mysteryWord = ["f", "o", "x"];
 var guessedLetters = [];
 var rewardArray = [];
-var numberOfGuesses = 0;
+var allGuesses = [];
+var numGuesses = 0;
 
 var multiplyArray = function(array){
 	var sum = array.reduce(function(a, b) {
@@ -50,24 +58,35 @@ var reward = function(){
 
 
 var guessLetter = function(userGuess){
-	
-	var test = mysteryWord.indexOf(userGuess);
-	if (test >= 0){
-		console.log("You picked correctly - the letter is " + userGuess + " is in the mystery word!");
-		guessedLetters.push(userGuess);
-		reward();
-		if (mysteryWord.length === guessedLetters.length) {
-			// console.log("testing");
-			// console.log(rewardArray);
-			var finalReward = multiplyArray(rewardArray);
-			// console.log("The final reward is " + finalReward);
-			console.log("You've picked all the letters! They are " + guessedLetters + ". You have won " + finalReward + "Can you now pick the word?");
-		}
+	allGuesses.push(userGuess);
+
+	console.log(allGuesses);
+	var checkLetter = mysteryWord.indexOf(userGuess);
+	if (checkLetter >= 0){
+			console.log("You picked correctly - the letter is " + userGuess + " is in the mystery word!");
+			guessedLetters.push(userGuess);
+			reward();
+				if (mysteryWord.length === guessedLetters.length) {
+					var finalReward = multiplyArray(rewardArray);
+					numGuesses = 0;
+					console.log("You've picked all the letters! They are " + guessedLetters + ". You have won " + finalReward + " The game is now over! Can you now pick the word?");
+				}
 	}
 	else {
+		numGuesses = numGuesses + 1 ;
+		remainingGuesses = 6 - numGuesses;
 		console.log("Sorry the letter " + userGuess + " is not in the mystery word");
+		if (remainingGuesses > 0){
+		console.log("The number of guesses you have got wrong  is " + numGuesses + ". You have " + remainingGuesses + " left. Good luck!");
+		}
+		else {
+			numGuesses = 0;
+			console.log("Oh oh...You have no guesses left, which means the hangman wins and the game is over !");
+		}
+		return numGuesses;
+
 	}
-	console.log(test);
+	// console.log(test);
 	console.log(guessedLetters);
 };
 
@@ -75,6 +94,15 @@ guessLetter("o");
 guessLetter("p");
 guessLetter("f");
 guessLetter("x");
+guessLetter("c");
+guessLetter("d");
+guessLetter("e");
+// guessLetter("g");
+// guessLetter("h");
+// guessLetter("i");
+// guessLetter("j");
+guessLetter("x");
+
 
 multiplyArray(rewardArray);
 
